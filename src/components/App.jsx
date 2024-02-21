9b1d2192c2aa32fc2e12533d8c4683a09e7dc65b
Vladyslav Mykhalytskyi <vmykhali666@gmail.com>
2024-02-21 04:19:55 +0200

    fix warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,5 @@
-import { FriendList } from "./FriendList/FriendList";
-import { Profile } from "./Profile/Profile";
-import { Statistics } from "./Statistics/Statistics";
-import data from "data/data.json";
-import friends from "data/friends.json";
-import user from "data/user.json"
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
-import transactions from "data/transactions.json"
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from 'data/transactions.json';
 
 export const App = () => {
   return (
@@ -16,16 +10,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      {
-      // <Profile username={user["username"]} tag={user["tag"]} 
-      // location={user["location"]} avatar={user["avatar"]} stats={user["stats"]}/>
-      // <Statistics title={"upload stats"} stats={data}/>
-      // <FriendList friends={friends}/>
-      <TransactionHistory items={transactions}/>
-      }
+      {<TransactionHistory items={transactions} />}
     </div>
   );
 };
